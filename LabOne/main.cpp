e4c28e33675f22cd6abe16cedb7d77510cd603c3
Alex McGarity <amcgarity123@gmail.com>
2022-09-09 10:35:34 -0500

    first commit

--- LabOne/main.cpp
+++ LabOne/main.cpp
@@ -3,19 +3,19 @@
 #include "mbed.h"
 
 //Define the pins used for the 7-segment display
-DigitalOut a(p5);
-DigitalOut b(p6);
-DigitalOut c(p7);
-DigitalOut d(p8);
-DigitalOut e(p9);
-DigitalOut f(p10);
-DigitalOut g(p11);
+DigitalOut a(D5);
+DigitalOut b(D6);
+DigitalOut c(D7);
+DigitalOut d(D8);
+DigitalOut e(D9);
+DigitalOut f(D10);
+DigitalOut g(D11);
 
 //Define the pins used for the internal counter
-DigitalOut i0(p12);
-DigitalOut i1(p13);
-DigitalOut i2(p14);
-DigitalOut i3(p15);
+DigitalOut i0(D12);
+DigitalOut i1(D13);
+DigitalOut i2(D14);
+DigitalOut i3(D15);
 
 // main() runs in its own thread in the OS
 int main()
